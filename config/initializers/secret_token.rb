1102b74a24bc852b85c493cf84c51f9bddc9bbc4
Lydia <lydia.katsamberis@ubm.com>
2011-08-03 15:05:25 -0500

    initial commit; created rails app with haml html5-boilerplate

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FontBlender::Application.config.secret_token = 'fa528dd2994a2cc8d951cab79555b04568c3d7c46b6617411b623e5fe29cd9818f6a6cd579806c84fee1d04ca71aef66019439dc9f7554cdbc06990b716ecd12'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+FontBlender::Application.config.session_store :cookie_store, :key => '_font_blender_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# FontBlender::Application.config.session_store :active_record_store
